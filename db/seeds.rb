571b979e10ee60cc71ecc066aa6855113f45a774
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-09 16:14:19 +0100

    challenge done without css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  new_article = Article.new
+  new_article.title = Faker::Movies::HarryPotter.quote
+  new_article.content = Faker::TvShows::Friends.quote
+  new_article.save
+end
